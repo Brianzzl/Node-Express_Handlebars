7d788cc260ccd05d987b9ae9a96bba8eec1b383c
Zheli Zhu <brianzzl@hotmail.com>
2019-09-03 10:14:37 -0400

    push

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ var connection;
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    connection = mysql.createConnection({
+} else {
+  connection = mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
-    password:"",
+    password:"r00tr00t",
     database:"burgers_db"
   });
 };
